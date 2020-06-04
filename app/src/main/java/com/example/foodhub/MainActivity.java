99fa7d2bcdd4c6c54bd225f33a319e60aea9ce37
sasha.boriskin <brskn.lx@gmail.com>
2020-06-04 20:41:46 +0300

    I changed the button. Now it called btnToReg. And I added a new Login Activity.

--- app/src/main/java/com/example/foodhub/MainActivity.java
+++ app/src/main/java/com/example/foodhub/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.foodhub;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.foodhub.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    Button btnToReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnToReg = (Button) findViewById(R.id.btnToReg);
+        btnToReg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnToReg:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
+
+
+
+
